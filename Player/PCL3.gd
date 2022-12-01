38e430c136bcc9f3404858e0e4e622003e559099
keldong <keldong@iu.edu>
2022-11-30 22:01:50 -0500

    Changes

--- Player/PCL3.gd
+++ Player/PCL3.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
 onready var Player = load("res://Player/Player.tscn")
-var starting_position = Vector2(200,200)
-var first_time = true
-var door2 = true
+var starting_position = Vector2(60,50)
 
 func _ready():
 	pass
@@ -11,20 +9,21 @@ func _ready():
 func _physics_process(_delta):
 	if not has_node("Player"):
 		var player = Player.instance()
-		player.position = starting_position
-		player.name = "Player"
+		properties(player)
 		add_child(player)
 
 
-func _on_Door1_body_entered(body):
-	if body.name == "Player" and first_time:
-		starting_position = Vector2(3488, -768)
-		Global.time += 30
-		first_time = false
+func properties(player):
+	player.scale.x = 0.4
+	player.scale.y = 0.4
+	player.position = starting_position
+	player.name = "Player"
+	
+	player.move_speed = 15
+	player.max_move = 150
 
+	player.jump_speed = 125
+	player.max_jump = 500
 
-func _on_Door2_body_entered(body):
-	if body.name == "Player" and door2:
-		starting_position = Vector2(160, -1472)
-		Global.time += 20
-		door2 = false
+	player.leap_speed = 125
+	player.max_leap = 600
